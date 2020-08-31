c6aaa3b8fdcdd51762a59e1330c0a15bc15542bc
Oreo-8 <2546663958@qq.com>
2020-08-31 17:29:13 +0800

    主页bar

---
+++ src/components/navigationBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="my-bar">
+    <a-menu v-model="current" mode="horizontal">
+      <a-menu-item key="mail">
+        <a-icon type="home" />主页
+      </a-menu-item>
+      <a-menu-item key="mail1">
+        <a-icon type="home" />分类
+      </a-menu-item>
+      <a-menu-item key="mail2">
+        <a-icon type="home" />收藏
+      </a-menu-item>
+      <a-menu-item key="mail3">
+        <a-icon type="home" />设置
+      </a-menu-item>
+      <a-menu-item key="mail4">
+        <a-icon type="home" />管理
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      current: ["mail"],
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="mainindex">
+    <a-layout>
+      <a-layout-header class="i-header">
+        <a-row style="height: 100%;">
+          <a-col class="col-1" :span="12">col-12</a-col>
+          <a-col class="col-2" :span="12">
+            <navigation-bar class="n-bar" />
+          </a-col>
+        </a-row>
+      </a-layout-header>
+      <a-layout-content>
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script>
+import navigationBar from "@/components/navigationBar";
+export default {
+  components: {
+    navigationBar,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.i-header {
+  padding: 0;
+}
+.col-1 {
+  height: 100%;
+}
+.col-2 {
+  height: 100%;
+  position: relative;
+}
+.n-bar {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
